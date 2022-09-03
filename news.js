ef92b5cce78e0cc6e41ca958fdc59f673197bfc8
Sayem92 <sayemmiha123@gmail.com>
2022-09-03 13:05:49 +0600

    add modal , title, details

--- news.js
+++ news.js
@@ -36,7 +36,7 @@ const displayInformation = (data) => {
     const containerlist = document.getElementById('list-container')
     containerlist.innerHTML = '';
     data.forEach(news => {
-        // console.log(news)
+        //  console.log(news)
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card card-side bg-base-300 p-8 mb-6 mx-10">
@@ -53,9 +53,9 @@ const displayInformation = (data) => {
                         </div>
                 </div>
                         <div>
-                            <h4 class="font-semibold text-2xl"><i class="fa-thin fa-eye"></i> ${news.total_view}M</h4>
+                            <h4 class="font-semibold text-2xl"><i class="fa-thin fa-eye"></i> ${news.total_view ? news.total_view : 'No'} M</h4>
                         </div>
-                <button class="btn btn-primary">Details</button>
+                <label for="my-modal-6" onclick="loadModalDetails('${news._id}')" class="btn btn-primary modal-button">Details</label>
             </div>
         </div>
     </div>
@@ -65,4 +65,57 @@ const displayInformation = (data) => {
 
 }
 
+
+
+const loadModalDetails = (id) =>{
+    // console.log(id)
+    const url = `https://openapi.programming-hero.com/api/news/${id}`
+    fetch(url)
+        .then(res => res.json())
+        .then(data => displayModal(data.data[0]))
+        .catch(error => console.log(error))
+
+}
+
+
+const displayModal = (detailsAll) =>{
+    console.log(detailsAll)
+    
+    const titleText = document.getElementById('title-text')
+    titleText.innerText = detailsAll.title;
+
+   
+
+    const infoAll = document.getElementById('details-all')
+    infoAll.innerText = detailsAll.details;
+
+}
+
+
 loadNews()
+
+
+
+
+// <input type="checkbox" id="my-modal-3" class="modal-toggle" />
+//     <div class="modal">
+//         <div class="modal-box relative h-full">
+//             <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
+//             <h3 class="text-lg font-bold">${detailsAll.title}</h3>
+
+//         <div class="card-actions justify-around pt-12">
+//                 <div class="flex gap-4">
+//                              <img class="rounded-full w-10" src="${detailsAll.author.img}" />
+//                         <div>
+//                             <p>${detailsAll.author.name ? detailsAll.author.name : 'No found'}</p>
+//                             <p>${detailsAll.author.published_date}</p>
+//                         </div>
+//                  </div>
+//                         <div>
+//                             <h4 class="font-semibold text-2xl"><i class="fa-thin fa-eye"></i> ${detailsAll.total_view ? detailsAll.total_view : 'No'} M</h4>
+//                         </div>
+//         </div>
+
+//             <p class="py-4">${detailsAll.details}</p>
+//         </div>
+//     </div>
