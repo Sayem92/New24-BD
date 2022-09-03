1869ac107957ced5f6e3e2740642082f9f7456b8
Sayem92 <sayemmiha123@gmail.com>
2022-09-03 23:38:19 +0600

    add sort view

--- news.js
+++ news.js
@@ -45,7 +45,11 @@ const displayCategoryIdLoad = (categoryId) => {
 
 
 const displayInformation = (data) => {
-    // console.log(data)
+     console.log(data)
+     
+         data.sort((a, b) => {
+             return b.total_view - a.total_view;
+             });
     // console.log(data.length)
     toggleSpinner(false)
     const textView = document.getElementById("text-view")
@@ -75,7 +79,7 @@ const displayInformation = (data) => {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
                             <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                           </svg></h4>
-                            <h4 class="font-semibold text-2xl">${news.total_view ? news.total_view : 'No'} M</h4>
+                            <h4 class="font-semibold text-2xl">${news.total_view ? news.total_view : 'No found'} M</h4>
                         </div>
                         <div  class="flex pt-4">
                             <h1>
