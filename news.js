aeed4c03b8e3542626889887e46a6a1ff323f89c
Sayem92 <sayemmiha123@gmail.com>
2022-09-03 20:28:38 +0600

    add sort view

--- news.js
+++ news.js
@@ -12,7 +12,7 @@ const displayCategory = (categories) => {
         // console.log(category)
         const li = document.createElement('li')
         li.innerHTML = `
-        <li onclick="displayCategoryIdLoad(${category.category_id})">${category.category_name}</li>
+        <li class="hover:text-primary hover:font-bold" onclick="displayCategoryIdLoad(${category.category_id})">${category.category_name}</li>
         `;
         menuList.appendChild(li)
       
@@ -52,8 +52,9 @@ const displayInformation = (data) => {
     textView.innerHTML = `${data.length ? data.length : 'No'} items found for category Entertainment`;
     const containerlist = document.getElementById('list-container')
     containerlist.innerHTML = '';
+    
     data.forEach(news => {
-        //  console.log(news)
+         console.log(news)
         const div = document.createElement('div')
         div.innerHTML = `
         <div class="card card-side bg-base-300 p-8 mb-6 mx-10 grid sm:grid-cols-1 md:grid-cols-2">
@@ -69,10 +70,43 @@ const displayInformation = (data) => {
                             <p>${news.author.published_date}</p>
                         </div>
                 </div>
-                        <div>
-                            <h4 class="font-semibold text-2xl"><i class="fa-thin fa-eye"></i> ${news.total_view ? news.total_view : 'No'} M</h4>
+                        <div class="flex pt-3">
+                            <h4 class="pr-2 pt-1"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                          </svg></h4>
+                            <h4 class="font-semibold text-2xl">${news.total_view ? news.total_view : 'No'} M</h4>
                         </div>
-                <label for="my-modal-6" onclick="loadModalDetails('${news._id}')" class="btn btn-primary modal-button">Details</label>
+                        <div  class="flex pt-4">
+                            <h1>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                            </svg>
+                            </h1>
+                            <h1>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                            </svg>
+                            </h1>
+                            <h1>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                            </h1>
+                            <h1>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                            </h1>
+                            <h1>
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z" />
+                            </svg>
+                            </h1>
+                        </div>
+                        <h1 class="pt-12 sm:pl-80 md:pl-96">
+                        <label for="my-modal-6" onclick="loadModalDetails('${news._id}')" class="btn btn-primary modal-button">Details</label>
+                        </h1>
             </div>
         </div>
     </div>
@@ -96,13 +130,12 @@ const loadModalDetails = (id) =>{
 
 
 const displayModal = (detailsAll) =>{
-    console.log(detailsAll)
+    // console.log(detailsAll)
     
-    const titleText = document.getElementById('title-text')
-    titleText.innerText = detailsAll.title;
-
     const authorInfo = document.getElementById('author-info')
     authorInfo.innerHTML =`
+                            <img class="w-full" src="${detailsAll.image_url}" alt="">        
+                            <h1 class="py-4 font-bold text-lg">${detailsAll.title}</h1>
                  <div class="flex gap-4 ">
                                  <img class="rounded-full w-10" src="${detailsAll.author.img}" />
                         <div class="pl-6">
@@ -110,8 +143,15 @@ const displayModal = (detailsAll) =>{
                                 <p>${detailsAll.author.published_date}</p>
                         </div>
                    
-                         <div  class="pl-16">
-                                 <h4 class="font-semibold text-2xl"><i class="fa-thin fa-eye"></i> ${detailsAll.total_view ? detailsAll.total_view : 'No'} M</h4>
+                         <div  class="pl-16 flex">
+                                <h4 class="pr-2 pt-1">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
+                                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                               </svg>
+                                </h4>
+                                 <h4 class="font-semibold text-2xl">
+                                ${detailsAll.total_view ? detailsAll.total_view : 'No'} M</h4>
                          </div>
                  </div>
     `;
@@ -126,26 +166,6 @@ loadNews()
 
 
 
-
-// <input type="checkbox" id="my-modal-3" class="modal-toggle" />
-//     <div class="modal">
-//         <div class="modal-box relative h-full">
-//             <label for="my-modal-3" class="btn btn-sm btn-circle absolute right-2 top-2">✕</label>
-//             <h3 class="text-lg font-bold">${detailsAll.title}</h3>
-
-//         <div class="card-actions justify-around pt-12">
-//                 <div class="flex gap-4">
-//                              <img class="rounded-full w-10" src="${detailsAll.author.img}" />
-//                         <div>
-//                             <p>${detailsAll.author.name ? detailsAll.author.name : 'No found'}</p>
-//                             <p>${detailsAll.author.published_date}</p>
-//                         </div>
-//                  </div>
-//                         <div>
-//                             <h4 class="font-semibold text-2xl"><i class="fa-thin fa-eye"></i> ${detailsAll.total_view ? detailsAll.total_view : 'No'} M</h4>
-//                         </div>
-//         </div>
-
-//             <p class="py-4">${detailsAll.details}</p>
-//         </div>
-//     </div>
+// datas.sort((a, b) => {
+//     return b.propertyName - a.propertyName;
+//     });
